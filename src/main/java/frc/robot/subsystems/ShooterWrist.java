06dd2c10eefe0a273e168780339f1b60d8517658
First1764 <first1764@gmail.com>
2026-01-12 20:11:11 -0600

    I added some  code for setting wrist angles

--- src/main/java/frc/robot/subsystems/ShooterWrist.java
+++ src/main/java/frc/robot/subsystems/ShooterWrist.java
@@ -4,8 +4,11 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterWrist extends SubsystemBase {
@@ -17,5 +20,29 @@ public class ShooterWrist extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  private double calculation;
+  PIDController controller = new PIDController(,, );
+// LOOK INTO: above line is missing values becasue I am not sure what values to insert, nor do i just want to copy previous values
+
+  public void flex(double angle){
+    if (wristMotor.getPosition().getValueAsDouble() > 300 || wristMotor.getPosition().getValueAsDouble() < 10){
+      wristMotor.set(0);
+    }
+    else {
+      calculation = controller.calculate(wristMotor.getPosition(), angle)
+      SmartDashboard.putNumber("SHOOTER_WRIST_PID", angle);
+      wristMotor.set(calculation);
+      // LOOK INTO: i don't know what to do abt calculate error or if my string is correct for smartdashboard, come back to it later
+    }
+  }
+
+  public void startFlex(){
+    wristMotor.set(.1); // this value is just a filler
+  }
+
+  public void stopFlex(){
+    wristMotor.set(0); 
   }
 }
