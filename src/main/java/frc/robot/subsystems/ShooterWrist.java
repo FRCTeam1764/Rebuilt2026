ece88fbf2fbc4fd4ee12f9a6a2dc783a5b58d296
First1764 <first1764@gmail.com>
2026-01-13 17:43:53 -0600

    added sysid and PID, as well as smartdashboard information

--- src/main/java/frc/robot/subsystems/ShooterWrist.java
+++ src/main/java/frc/robot/subsystems/ShooterWrist.java
@@ -3,38 +3,72 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
+import static edu.wpi.first.units.Units.Volts;
 
+import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
 public class ShooterWrist extends SubsystemBase {
   /** Creates a new ShooterWrist. */
-  public ShooterWrist() {}
 
   TalonFX wristMotor = new TalonFX(27);
-  
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
 
   private double calculation;
-  PIDController controller = new PIDController(,, );
-// LOOK INTO: above line is missing values becasue I am not sure what values to insert, nor do i just want to copy previous values
+  private VoltageOut voltageOut;
+  private StateManager stateManager;
+  private PIDController controller = new PIDController(,,);
+// PID values, do them
+
+  private final SysIdRoutine m_sysIdRoutine = 
+    new SysIdRoutine(
+      new SysIdRoutine.Config(
+        null,
+        Volts.of(4),
+        null,
+
+        (state) -> SignalLogger.writeString("state", state.toString())
+      ),
+      new SysIdRoutine.Mechanism(
+        (volts) -> wristMotor.setControl(voltageOut.withOutput(volts.in(Volts))),
+        null,
+        this
+      )
+    );
+
+    public ShooterWrist(StateManager stateManager) {
+
+      this.stateManager = stateManager;
+      
+
+
+    }
+
+
+  public Command sysIdQuasistatic(SysIdRoutine.Direction direction){
+    return m_sysIdRoutine.quasistatic(direction);
+  }
+
+  public Command sysIdDynamic(SysIdRoutine.Direction direction){
+    return m_sysIdRoutine.dynamic(direction);
+  }
+  
 
   public void flex(double angle){
     if (wristMotor.getPosition().getValueAsDouble() > 300 || wristMotor.getPosition().getValueAsDouble() < 10){
       wristMotor.set(0);
     }
     else {
-      calculation = controller.calculate(wristMotor.getPosition(), angle)
+      calculation = controller.calculate(wristMotor.getPosition().getValueAsDouble(), angle);
       SmartDashboard.putNumber("SHOOTER_WRIST_PID", angle);
       wristMotor.set(calculation);
-      // LOOK INTO: i don't know what to do abt calculate error or if my string is correct for smartdashboard, come back to it later
     }
   }
 
@@ -44,5 +78,20 @@ public class ShooterWrist extends SubsystemBase {
 
   public void stopFlex(){
     wristMotor.set(0); 
+  }
+
+
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("ShooterWristPosition",wristMotor.getPosition().getValueAsDouble());
+    SmartDashboard.putNumber("ShooterWristTemperature", wristMotor.getDeviceTemp().getValueAsDouble());
+    SmartDashboard.putNumber("ShooterWristCurrent", wristMotor.getStatorCurrent().getValueAsDouble());
+    SmartDashboard.putBoolean("ShooterWristHappy", wristMotor.getStatorCurrent().getValueAsDouble()<30);
+    // value 30 in above line is pulled from reefscape intake wrist, might have to change later
+    SmartDashboard.putNumber("UnhappyCount", SmartDashboard.getNumber("UnhappyCount", 0)); // reefscape code also included a check for if the elevator was happy
+    // This method will be called once per scheduler run
+
+    // more code here in reefscape with stagemanager, prob have to add later
   }
 }
